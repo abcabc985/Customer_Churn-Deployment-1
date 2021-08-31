2e606bf7856ad9eab3e5b9dc4322d8cc82128966
abcabc985 <89873152+abcabc985@users.noreply.github.com>
2021-08-31 17:01:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def predict():
              return render_template('index.html',prediction_text="The Customer will not leave the bank")
                 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
